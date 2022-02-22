157c14757f0d245f2bc2d6aa3e42a7a010e429c7
WooriPbg <wooripbg@gmail.com>
2022-02-22 15:15:28 +0900

    라이선스 변경(유효기간 2022년 4월 1일)

--- report-guide/js/realreport/realreport-lic.js
+++ report-guide/js/realreport/realreport-lic.js
@@ -1,2 +1,2 @@
-// 아래 라이선스는 유효기간 2022년 2월 17일 까지 localhost 또는 real-report.com 도메인에서 사용할 수 있는 라이선스 입니다.
-var realReportLic = 'upVcPE+wPOkcfqywe+clVN+UVTCvO3is+83EYTz6U/sTXJR8Yw8Y0WXyjOMqbrgvr3+iyFPC2UvWntFnlQAvG/WiN+dO0JtjVohH/45jQUB5oxsrC+spwoIBzPQYFtNP+6ZGVxDxR7Q='
+// 아래 라이선스는 유효기간 2022년 4월 1일 까지 localhost 또는 real-report.com 도메인에서 사용할 수 있는 라이선스 입니다.
+var realReportLic = 'upVcPE+wPOkcfqywe+clVN+UVTCvO3is+83EYTz6U/sTXJR8Yw8Y0WXyjOMqbrgvyJiJd30ovvcYmXbwCWu68DU2pJNMkfVHMirU+qBnwfvRB4hu3LIgQZMVWpaY093J'
