54e96671feeb09fa3b78aea94da711354e96e2c4
onlydel <onlydel@wooritech.com>
2022-03-02 19:57:38 +0900

    미리보기 툴바 추가

--- html/js/app.js
+++ html/js/app.js
@@ -1,7 +1,7 @@
 // 아래 라이선스는 유효기간 만료된 라이선스
 // var realReportLic = 'upVcPE+wPOkcfqywe+clVN+UVTCvO3is+83EYTz6U/sTXJR8Yw8Y0adoq8hudFeXNTakk0yR9UevUdDFVwVRcNEHiG7csiBBkxValpjT3ck='
-// 아래 라이선스는 유효기간 2022년 3월 31일 까지 localhost, 127.0.0.1 또는 real-report.com 도메인에서 사용할 수 있는 라이선스 입니다.
-var realReportLic = 'upVcPE+wPOkcfqywe+clVN+UVTCvO3is+83EYTz6U/sTXJR8Yw8Y0WXyjOMqbrgvr3+iyFPC2UvWntFnlQAvG/WiN+dO0JtjVohH/45jQUB5oxsrC+spwqSi7PsusB6k+6ZGVxDxR7Q=';
+// 아래 라이선스는 유효기간 2022년 4월 1일 까지 localhost, 127.0.0.1 또는 real-report.com 도메인에서 사용할 수 있는 라이선스 입니다.
+var realReportLic = 'upVcPE+wPOkcfqywe+clVN+UVTCvO3is+83EYTz6U/sTXJR8Yw8Y0WXyjOMqbrgvr3+iyFPC2UvWntFnlQAvG/WiN+dO0JtjVohH/45jQUB5oxsrC+spwsWvPAQ1rit9+6ZGVxDxR7Q=';
 
 var viewer;
 var currentId = '';
