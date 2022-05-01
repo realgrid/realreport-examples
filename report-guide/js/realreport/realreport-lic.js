418913b3254f5f03441647c1e647363d07b44991
onlydel <onlydel@wooritech.com>
2022-05-02 08:40:06 +0900

    라이선스 업데이트

--- report-guide/js/realreport/realreport-lic.js
+++ report-guide/js/realreport/realreport-lic.js
@@ -1,2 +1,2 @@
-// 아래 라이선스는 유효기간 2022년 5월 1일 까지 localhost 또는 real-report.com 도메인에서 사용할 수 있는 라이선스 입니다.
-var realReportLic = 'upVcPE+wPOkcfqywe+clVN+UVTCvO3is+83EYTz6U/sTXJR8Yw8Y0WXyjOMqbrgvr3+iyFPC2UvWntFnlQAvG/WiN+dO0JtjVohH/45jQUB5oxsrC+spwi8zdmpxDKOd+6ZGVxDxR7Q=';
+// 아래 라이선스는 유효기간 2022년 6월 1일 까지 localhost, 127.0.0.1 또는 real-report.com 도메인에서 사용할 수 있는 라이선스 입니다.
+var realReportLic = 'upVcPE+wPOkcfqywe+clVN+UVTCvO3is+83EYTz6U/sTXJR8Yw8Y0WXyjOMqbrgvr3+iyFPC2UvWntFnlQAvG/WiN+dO0JtjdX/TRvH17asONaYpJP0FO3omxwgpdzq2ZpbS0uUeOfM1/Z20sDsGHv75qiwBaTr7';
