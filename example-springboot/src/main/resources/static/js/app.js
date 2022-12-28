e3740e9b6a8fa8af8c1239fdbe8db7a8cee61cd4
realgrid <>
2022-12-28 07:21:16 +0000

    Merge pull request #181 from realgrid/yesjean/fix-lic
    라이선스 오류로 교체

--- example-springboot/src/main/resources/static/js/app.js
+++ example-springboot/src/main/resources/static/js/app.js
@@ -1,5 +1,5 @@
 // 아래 라이선스는 유효기간 2023년 2월 1일 까지 localhost, 127.0.0.1 또는 real-report.com 도메인에서 사용할 수 있는 라이선스 입니다.
-var realReportLic = 'upVcPE+wPOkcfqywe+clVN+UVTCvO3is+83EYTz6U/sTXJR8Yw8Y0WXyjOMqbrgvr3+iyFPC2UtN2Jewv14U7PWiN+dO0JtjdX/TRvH17asONaYpJP0FO3omxwgpdzq2I8zB1XKm5AA1/Z20sDsGHv75qiwBaTr7';
+var realReportLic = 'upVcPE+wPOkcfqywe+clVN+UVTCvO3is+83EYTz6U/sTXJR8Yw8Y0WXyjOMqbrgvr3+iyFPC2UvWntFnlQAvG/WiN+dO0JtjdX/TRvH17asONaYpJP0FO3omxwgpdzq2I8zB1XKm5AA1/Z20sDsGHv75qiwBaTr7';
 var viewer;
 var editor;
 
