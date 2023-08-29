ea94fa5276ab643065fa4091b4947d4d9c11d354
realgrid <>
2023-08-29 02:44:08 +0000

    Merge pull request #255 from realgrid:dev
    데모사이트 라이선스 업데이트

--- report-viewer-html/js/app.js
+++ report-viewer-html/js/app.js
@@ -1,8 +1,7 @@
 // import './templates/index.js';
 
-// 아래 라이선스는 유효기간 2023년 9월 1일 까지 localhost,127.0.0.1,-.real-report.com,*.realgrid.com,*.vercel.app 도메인에서 사용할 수 있는 라이선스 입니다.
-var realReportLic =
-  'upVcPE+wPOkcfqywe+clVN+UVTCvO3is+83EYTz6U/sTXJR8Yw8Y0WXyjOMqbrgvr3+iyFPC2UtN2Jewv14U7PWiN+dO0JtjdX/TRvH17asONaYpJP0FO3fFyQNoWuTp2ZPD4xNAYbs1NqSTTJH1R7+1P9h/fjdG0QeIbtyyIEGTFVqWmNPdyQ==';
+// 아래 라이선스는 유효기간 2023년 10월 1일 까지 localhost, 127.0.0.1 또는 real-report.com 도메인에서 사용할 수 있는 라이선스 입니다.
+var realReportLic = 'upVcPE+wPOkcfqywe+clVN+UVTCvO3is+83EYTz6U/sTXJR8Yw8Y0WXyjOMqbrgvr3+iyFPC2UvWntFnlQAvG/WiN+dO0JtjdX/TRvH17asONaYpJP0FO3fFyQNoWuTp2ZPD4xNAYbs1NqSTTJH1R719Q6KeXmwKQbZCTWbp93HQX/333q4sjQ==';
 var viewer;
 var editor;
 
