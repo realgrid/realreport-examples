e4938d90921a957afbdcd8b9ff734e324ee8e767
onlydel <onlydel@wooritech.com>
2025-06-11 08:05:59 +0000

    Deploying to gh-pages from @ realgrid/realreport-examples@ecc168e55b28c929d7aa2252ac69b5d69417e08b 🚀

--- js/realmap/realmap.lic.js
+++ js/realmap/realmap.lic.js
@@ -1,2 +1,3 @@
+// 2025.09.11 까지 demo.realreport.co.kr,demo.real-report.com,127.0.0.1,localhost에서 사용가능한 RealMap 라이선스입니다.
 var realMapLic =
-    'upVcPE+wPOl3H1t2EmYDS2ZHHsIGhYOFvAiOqCN7jMR+G9Zkz21+pZvoHKp5iqjbBpI3wP4w7/P0ZXA6OWCrCykq1gwG8v2FqHSOtMOyiX5PjgWEMs4moeK/TGVxDoYg';
+    'upVcPE+wPOl3H1t2EmYDS2ZHHsIGhYOFvAiOqCN7jMR26g2erGWtAGr0tQnb9AArqZFDT86YfPm+9Ot/AUWfApbNKlJ0uYxkciCj0MRh+Cxl8ozjKm64L69/oshTwtlLyqrdX9J2KtDlww1wBBwQWsPKu0ZmtwfEi8lZLyYbnWYB9nScXFoNRqrdTN78LCOKnj7uPVYneq0=';
