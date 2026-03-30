5886b1e1a96bfd962514a499ad1db84d553eeb1c
realgrid <onlydel@wooritech.com>
2026-03-30 04:53:30 +0000

    Chore: realreport, realmap 데모용 라이센스 업데이트

--- report-viewer-html/js/realmap/realmap.lic.js
+++ report-viewer-html/js/realmap/realmap.lic.js
@@ -1,3 +1,3 @@
-// 2026.01.15 까지 demo.realreport.co.kr,demo.real-report.com,127.0.0.1,localhost에서 사용가능한 RealMap 라이선스입니다.
+// 2026.12.31 까지 demo.realreport.co.kr,demo.real-report.com,127.0.0.1,localhost에서 사용가능한 RealMap 라이선스입니다.
 var realMapLic =
-    'upVcPE+wPOl3H1t2EmYDS2ZHHsIGhYOFvAiOqCN7jMR26g2erGWtAGr0tQnb9AArqZFDT86YfPm+9Ot/AUWfApbNKlJ0uYxkbRn8zx/z+OWLmrqK4xhR4sS/Ob2yaOICc6qfV6+LrnoTDiQEqGbT6sYgASwEn2ZSZptqQpoJk7T0DqdSjq7qLi24tjaDjOJEYAPKxvqP4Ac=';
+    'upVcPE+wPOl3H1t2EmYDS2ZHHsIGhYOFvAiOqCN7jMR26g2erGWtAGr0tQnb9AArqZFDT86YfPm+9Ot/AUWfApbNKlJ0uYxkbRn8zx/z+OWLmrqK4xhR4sS/Ob2yaOICc6qfV6+Lrnpy00lZ6ljxM795NbcAqKR+R31vu2UKKuD3Xa1sTBDpMpN5ApCInAFpl5GWy+kng6E=';
